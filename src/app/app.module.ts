b074e271ec750fa7fb87fe9ac8c58096be97ac7e
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-11 20:31:29 +0600

    create component app-burger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BurgerComponent } from './burger/burger.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgerComponent,
+    IngredientsComponent
   ],
   imports: [
     BrowserModule
