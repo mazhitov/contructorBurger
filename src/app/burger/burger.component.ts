e5d1ba225cbaa8b3aad0715d3f3ea3569fb2f67c
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-11 22:33:34 +0600

    add burger style.css

--- src/app/burger/burger.component.ts
+++ src/app/burger/burger.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Ingredient} from "../../shared/Ingredient.model";
 
 @Component({
   selector: 'app-burger',
   templateUrl: './burger.component.html',
   styleUrls: ['./burger.component.css']
 })
-export class BurgerComponent implements OnInit {
+export class BurgerComponent{
+  @Input() ingredients!:Ingredient[];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
