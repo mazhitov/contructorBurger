ea8a1c0a0779b7bddaad8a20cf8fdf43de38335d
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-11 22:33:13 +0600

    create ingredients of class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Ingredient} from "../shared/Ingredient.model";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Meat', 0, 50, '../../assets/burger/meat.png'),
+    new Ingredient('Cheese', 0, 20, '../../assets/burger/cheese.png'),
+    new Ingredient('Salad', 0, 5, '../../assets/burger/salad.png'),
+    new Ingredient('Bacon', 0, 30, '../../assets/burger/bacon.png'),
+  ];
 
 }
