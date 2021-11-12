a71b9675c0669a1d3197990ab4b2b594e82fde4c
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-11-12 17:21:55 +0600

    add price of Burger to DOM

--- src/app/burger/price/price.component.ts
+++ src/app/burger/price/price.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Ingredient} from "../../../shared/Ingredient.model";
 
 @Component({
   selector: 'app-price',
   templateUrl: './price.component.html',
   styleUrls: ['./price.component.css']
 })
-export class PriceComponent implements OnInit {
+export class PriceComponent  {
+  @Input() ingredients:Ingredient[] = [];
+  burgerPrice = 20;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getTotal() {
+    return this.ingredients.reduce((acc, item) => {
+      return acc + item.getPrice();
+    }, 0);
   }
-
+  getPrice() {
+    return this.getTotal() + this.burgerPrice;
+  }
 }
